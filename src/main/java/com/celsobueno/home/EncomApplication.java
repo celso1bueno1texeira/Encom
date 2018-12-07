208b94da31b7bfef754eb87a57044717529bf7c3
Celso Bueno <cbtbueno@gmail.com>
2018-12-07 16:28:08 -0300

    Criação do Projeto

---
+++ src/main/java/com/celsobueno/home/EncomApplication.java
@@ -0,0 +1,12 @@
+package com.celsobueno.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncomApplication.class, args);
+	}
+}
