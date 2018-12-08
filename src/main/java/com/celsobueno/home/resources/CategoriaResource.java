6d2c981ae48db5ade0346794ca4e9d119d131e81
Celso Bueno <cbtbueno@gmail.com>
2018-12-07 22:57:44 -0300

    Implementando Classe Categoria

--- src/main/java/com/celsobueno/home/resources/CategoriaResource.java
+++ src/main/java/com/celsobueno/home/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.celsobueno.home.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.celsobueno.home.domain.Categoria;
 
 @RestController	
 @RequestMapping(value="/categoria")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return" REST está funcionando";
+	public List<Categoria> list() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+ 		return lista;
 	}
 	
 
