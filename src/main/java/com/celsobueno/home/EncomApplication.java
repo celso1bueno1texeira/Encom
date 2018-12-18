29c8f3c322a44a2c47b7290ee3f2a05d2966999a
Celso Bueno <cbtbueno@gmail.com>
2018-12-18 12:33:57 -0300

    Criando operação de instanciação

--- src/main/java/com/celsobueno/home/EncomApplication.java
+++ src/main/java/com/celsobueno/home/EncomApplication.java
@@ -1,12 +1,38 @@
 package com.celsobueno.home;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.celsobueno.home.domain.Categoria;
+import com.celsobueno.home.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class EncomApplication {
+public class EncomApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncomApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,  "Informática");
+		Categoria cat2 = new Categoria(null,  "Escritório");
+		Categoria cat3 = new Categoria(null,  "Papelaria");	
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+	}
+	 	
+	
+	
+
+	
+	
 }
