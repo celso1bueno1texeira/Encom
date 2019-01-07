b4b3496e84f9288348aae6defe5387ea0ea11cf0
Celso Bueno <cbtbueno@gmail.com>
2019-01-07 20:53:31 -0300

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/celsobueno/home/resources/CategoriaResource.java
+++ src/main/java/com/celsobueno/home/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.celsobueno.home.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,14 +23,9 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 		
-		/*Categoria cat1 = new Categoria(1, "Informática");
-		**Categoria cat2 = new Categoria(2, "Escritório");
-		**
-		**List<Categoria> lista = new ArrayList<>();
-		**lista.add(cat1);
-		**lista.add(cat2);
 		
- 		*return lista;*/
+		
+		
 	}
 	
 
