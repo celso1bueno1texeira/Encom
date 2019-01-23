4e6eddb17413e9e4134a42b1c4240ca66b5eef01
Celso Bueno <cbtbueno@gmail.com>
2019-01-22 21:40:16 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/celsobueno/home/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.celsobueno.home.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.celsobueno.home.domain.Cliente;
+import com.celsobueno.home.repository.ClienteRepository;
+import com.celsobueno.home.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!!! Id: " + id + "Tipo: " + Cliente.class.getName()));
+		
+		
+		
+		
+		
+	}
+
+}
